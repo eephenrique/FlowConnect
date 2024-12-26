663577beed0bee4dc6838cfc55df140a30f0e83d
TBRWEB - Desenvolvimento de Sites <84810078+tbrweb-dev@users.noreply.github.com>
2024-12-26 17:37:28 -0300

    Module three B

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,7 +16,7 @@ export function Sidebar() {
             <Avatar src="https://images.unsplash.com/photo-1734000402740-dc480cbbaeb6?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
 
-            <strong>Marcos Henrique</strong>
+            <strong>Marcos Henrique.</strong>
             <span>Web Developer</span>
         </div>
 
